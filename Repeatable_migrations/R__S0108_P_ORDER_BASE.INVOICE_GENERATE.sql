df8b1b89a857ab37fa615d4ace94c6cc1227b95f
yogeshwaran <yogeshwaran.mohan@ideas2it.com>
2024-05-30 19:10:40 +0530

    Implemented standard names and files

--- Repeatable_migrations/R__S0108_P_ORDER_BASE.INVOICE_GENERATE.sql
+++ Repeatable_migrations/R__S0108_P_ORDER_BASE.INVOICE_GENERATE.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE ORDER_BASE.INVOICE_GENERATE()
+CREATE PROCEDURE INTEL_ASSESMENT."ORDER_BASE".INVOICE_GENERATE()
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
 EXECUTE AS CALLER
@@ -20,25 +20,25 @@ BEGIN
         CREATE_USER
     )
     SELECT 
-    od.ORDER_ID,
-    oh.ORDER_DATE AS ORDER_DATE_HEADER,
-    od.create_date,
-    0.09 AS TAX_AMOUNT,
-    oh.TOTAL_AMOUNT,
-    oh.DISCOUNT,
-    (oh.total_amount + 0.09) - oh.discount AS FINAL_INVOICE_AMT,
-    oh.CUSTOMER_ID,
-    oh.CREATE_DATE AS CREATE_DATE,
-    oh.CREATE_USER
-FROM 
-    ORDER_BASE.ORDER_HEADER oh
-JOIN 
-    ORDER_BASE.ORDER_DETAIL od ON oh.customer_id = od.customer_id
-JOIN 
-    ORDER_BASE.CUSTOMER c ON oh.CUSTOMER_ID = c.CUSTOMER_ID
-WHERE 
-    oh.ORDER_DATE > CURRENT_DATE() AND
-    c.STATUS = ''Active'';
+        od.ORDER_ID,
+        oh.ORDER_DATE AS ORDER_DATE_HEADER,
+        od.create_date,
+        oh.TOTAL_AMOUNT * 0.09 AS TAX_AMOUNT,
+        oh.TOTAL_AMOUNT,
+        oh.DISCOUNT,
+        (oh.total_amount + 0.09) - oh.discount AS FINAL_INVOICE_AMT,
+        oh.CUSTOMER_ID,
+        oh.CREATE_DATE AS CREATE_DATE,
+        oh.CREATE_USER
+    FROM 
+        ORDER_BASE.ORDER_HEADER oh
+    JOIN 
+        ORDER_BASE.ORDER_DETAIL od ON oh.customer_id = od.customer_id
+    JOIN 
+        ORDER_BASE.CUSTOMER c ON oh.CUSTOMER_ID = c.CUSTOMER_ID
+    WHERE 
+        oh.ORDER_DATE > CURRENT_DATE() AND
+        c.STATUS = ''Active'';
 
 
     RETURN ''Invoice data inserted successfully'';
